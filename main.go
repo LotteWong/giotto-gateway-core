24137451f4160d79ded8a82806f7945a8c34e5ee
lottewong <lottewong21@gmail.com>
2021-03-18 17:13:25 +0800

    feat: support management endpoint and proxy endpoint
    1. support different configs and endpoints
    2. enable both http and https proxy server

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"flag"
+	http_proxy_router "github.com/LotteWong/giotto-gateway/http_proxy_router"
 	"github.com/LotteWong/giotto-gateway/router"
 	"github.com/e421083458/golang_common/lib"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+var (
+	endpoint = flag.String("endpoint", "", "management or proxy")
+	config   = flag.String("config", "", "config file path")
+)
+
 func main() {
-	lib.InitModule("./configs/dev/", []string{"base", "mysql", "redis"})
+	flag.Parse()
+	if *endpoint == "" || *config == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch *endpoint {
+	case "management":
+		InitManagementServer(*config)
+	case "proxy":
+		InitProxyServer(*config)
+	default:
+		log.Fatalf(" [ERROR] endpoint %s is invalid", *endpoint)
+	}
+}
+
+func InitManagementServer(config string) {
+	lib.InitModule(config, []string{"base", "mysql", "redis"})
 	defer lib.Destroy()
+
 	router.HttpServerRun()
 
 	quit := make(chan os.Signal)
@@ -18,4 +44,23 @@ func main() {
 	<-quit
 
 	router.HttpServerStop()
+}
+
+func InitProxyServer(config string) {
+	lib.InitModule(config, []string{"base", "mysql", "redis"})
+	defer lib.Destroy()
+
+	go func() {
+		http_proxy_router.HttpServerRun()
+	}()
+	go func() {
+		http_proxy_router.HttpsServerRun()
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	http_proxy_router.HttpServerStop()
+	http_proxy_router.HttpsServerStop()
 }
