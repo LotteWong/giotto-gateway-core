b1b55f5030feb45a23d7d8fbb0a296f61990b13d
lottewong <lottewong21@gmail.com>
2021-03-22 21:06:40 +0800

    feat: apply http ip auth middleware
    1. enable both white ip list and black ip list on http proxy route
    2. enable both allow ip list and block ip list on management route

--- middleware/ip_auth.go
+++ middleware/ip_auth.go
@@ -1,22 +1,46 @@
 package middleware
 
 import (
-	"errors"
 	"fmt"
 	"github.com/e421083458/golang_common/lib"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
-func IPAuthMiddleware() gin.HandlerFunc {
+func IpAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		isMatched := false
-		for _, host := range lib.GetStringSliceConf("base.http.allow_ip") {
-			if c.ClientIP() == host {
+		var isMatched bool
+		var authMode string
+		allowIps := lib.GetStringSliceConf("base.http.allow_ips")
+		blockIps := lib.GetStringSliceConf("base.http.block_ips")
+
+		if len(allowIps) > 0 {
+			authMode = "allow"
+			for _, host := range allowIps {
+				if c.ClientIP() == host {
+					isMatched = true
+				}
+			}
+			isMatched = false
+		} else {
+			authMode = "block"
+			if len(blockIps) > 0 {
+				for _, host := range blockIps {
+					if c.ClientIP() == host {
+						isMatched = false
+					}
+				}
 				isMatched = true
 			}
 		}
-		if !isMatched{
-			ResponseError(c, InternalErrorCode, errors.New(fmt.Sprintf("%v, not in iplist", c.ClientIP())))
+
+		if !isMatched {
+			switch authMode {
+			case "allow":
+				ResponseError(c, InternalErrorCode, errors.New(fmt.Sprintf("ip %s not in allow ip list", c.ClientIP())))
+			case "block":
+				ResponseError(c, InternalErrorCode, errors.New(fmt.Sprintf("ip %s is in block ip list", c.ClientIP())))
+			}
 			c.Abort()
 			return
 		}
