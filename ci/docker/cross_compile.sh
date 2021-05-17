33c0ab0a47c695353f4e019dfc5fddcade13eafa
LotteWong <lottewong21@gmail.com>
2021-05-17 22:52:18 +0800

    ci: modify nohup/docker/k8s files

--- ci/docker/cross_compile.sh
+++ ci/docker/cross_compile.sh
@@ -6,9 +6,9 @@ export GOPROXY=https://goproxy.io,direct
 go mod tidy
 
 # build binary executable
-mkdir -p ../bin/giotto_gateway_core
-GOOS=linux GOARCH=amd64 go build -o ../bin/giotto_gateway_core
+mkdir -p ./bin
+GOOS=linux GOARCH=amd64 go build -o ./bin/giotto_gateway_core
 
 # build docker images
-commit=echo `git rev-parse --short HEAD`
-docker build -f Dockerfile -t giotto-gateway-core:$commit ..
+commit=`git rev-parse --short HEAD`
+docker build -f ./ci/docker/Dockerfile -t giotto-gateway-core:$commit .
--- ci/nohup/start.sh
+++ ci/nohup/start.sh
@@ -6,12 +6,12 @@ export GOPROXY=https://goproxy.io,direct
 go mod tidy
 
 # build binary executable
-mkdir -p ../bin/giotto_gateway_core
-go build -o ../bin/giotto_gateway_core
+mkdir -p ./bin
+go build -o ./bin/giotto_gateway_core
 
 # kill processes already started
 pkill -9 giotto_gateway_core
 
 # run management backgroud server
-nohup ../bin/giotto_gateway_core -config ../config/prod/ >> logs/core.log 2>&1 &
-echo 'nohup ../bin/giotto_gateway_core -config ./config/prod/ >> logs/core.log 2>&1 &'
+nohup ./bin/giotto_gateway_core -config ./configs/prod/ >> ./logs/core.log 2>&1 &
+echo 'nohup ./bin/giotto_gateway_core -config ./configs/prod/ >> ./logs/core.log 2>&1 &'
